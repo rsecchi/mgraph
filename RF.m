982a9f96d07cf774ed33345c007883663180c928
rsecchi <r.secchi@gmail.com>
2018-07-20 20:10:52 +0100

    empty RF script

---
+++ RF.m
@@ -0,0 +1,26 @@
+EPS=1e-7;
+
+% The function returns a T model of a reciprocal
+% passive network. 
+function m = tmodel(Z)
+end
+
+% The function returns a Pi model of a reciprocal
+% passive network
+function m = pmodel(Z)
+end
+
+% The function returns the Zin, Zout that maximise
+% the
+function [Zin, Zout] = adapt(Z)
+end
+
+% Maximum available gain
+function Ga = gain(Z)
+end
+
+% The function converts the Z matrix into an S matrix
+function S = ZtoS(Z,R=50)
+end
+
+% Gain
