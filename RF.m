09e81f8c76920db74f6b66044420d018febe9f1b
Raffaello Secchi <raffaello@android-7429fdd75f2e7e3b.home>
2018-07-21 22:20:39 +0100

    functions added

--- RF.m
+++ RF.m
@@ -52,8 +52,31 @@ function Ga = gain(Z)
 end
 
 % The function converts the Z matrix into an S matrix
-function S = ZtoS(Z,R0=50)
+function S = Z2S(Z,R0=50)
 	S = (Z-R0*eye(2))/(Z+R0*eye(2));
 end
 
-% Gain
+% The function converts the Z matrix into an S matrix
+function Z = S2Z(S, R0=50)
+	S = R0*(eye(2)+S)/(eye(2)-S);
+end
+
+% S to Transfer params
+function T = S2T(S)
+	T11 = -det(S)/S(2,1);
+	T12 = S(1,1)/S(2,1);
+	T21 = -S(2,2)/S(2,1);
+	T22 = 1/S(2,1);
+	T = [[T11 T12]; [T21 T22]];
+end
+
+% Transfer to S parameters
+function S = T2S(T)
+	S11 = T(1,2)/T(2,2);
+	S12 = det(T)/T(2,2);
+	S21 = 1/T(2,2);
+	S22 = -T(2,1)/T(2,2);
+	S = [[S11 S12]; [S21 S22]];
+end
+
+
